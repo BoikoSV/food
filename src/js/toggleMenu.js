6b69eb26ad67df79534a3c7e420dc8acbd665148
Sergiy Boiko <boikosmail2@gmail.com>
2021-07-10 16:14:58 +0300

    Finish the menu

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,13 +1,48 @@
 (function(){
     const menu = document.querySelector('.menu');
     const button = menu.querySelector('#hamburger-icon');
+    const menuList = menu.querySelector('.menu__list');
 
-    
-    button.addEventListener('click', function(){
-        toggleBtn();
-    })
 
     function toggleBtn(){
         button.classList.toggle('active');
     }
+
+    function hiddenBtn(){
+        if(document.documentElement.offsetWidth >= 769){
+            button.style.display = 'none';
+        }
+        if(document.documentElement.offsetWidth < 769){
+            button.style.display = 'inline-block';
+        }
+    }
+    hiddenBtn();
+
+    function toggleMenu(){
+        menuList.classList.toggle('header__menu-list--open');
+        if(menuList.classList.contains('header__menu-list--open')){
+            menuList.classList.remove('header__menu-list--close');
+        }else{
+            menuList.classList.add('header__menu-list--close');
+        }
+    }
+
+    function closeMenu(){
+        button.classList.remove('active');
+        menuList.classList.remove('header__menu-list--open');
+        menuList.classList.remove('header__menu-list--close');
+    }
+
+    button.addEventListener('click', function(){
+        toggleBtn();
+        toggleMenu();
+    })
+
+    window.addEventListener('resize', function(){
+        if(document.documentElement.clientWidth >= 769){
+            closeMenu();
+        }
+        hiddenBtn();
+    })
+
 })();
